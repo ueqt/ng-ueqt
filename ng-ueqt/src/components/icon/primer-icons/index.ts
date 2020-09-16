69ab09bc0df7c5997545f55cc57305912b509bc9
ueqt <ueqtxu@gmail.com>
2020-09-16 14:44:50 +0800

    fix: use all icons

--- ng-ueqt/src/components/icon/primer-icons/index.ts
+++ ng-ueqt/src/components/icon/primer-icons/index.ts
@@ -1,5 +1,5 @@
+export * from './chevron-down';
 export * from './chevron-left';
 export * from './chevron-right';
 export * from './chevron-up';
-export * from './chevron-down';
 export * from './three-bars';
--- src/demo/icon/basic.ts
+++ src/demo/icon/basic.ts
@@ -1,12 +1,23 @@
+import { UIconDefinition } from './../../../ng-ueqt/src/components/icon/icon.model';
 import { Component } from '@angular/core';
+import * as AllIcons from '../../../ng-ueqt/src/components/icon/primer-icons';
 
 @Component({
   selector: 'udemo-icon-basic',
-  template: ` <i uIcon="bars" style="color: red;"></i>
+  template: ` 
+  <i *ngFor="let icon of icons" [uIcon]="icon.name" style="color:red;"></i>
+  <!-- <i uIcon="bars" style="color: red;"></i>
     <i uIcon="left"></i>
     <i uIcon="right"></i>
     <i uIcon="up"></i>
     <i uIcon="down"></i>
-    <i uIcon="bars"></i>`,
+    <i uIcon="bars"></i> -->
+    `,
 })
-export class UdemoIconBasicComponent {}
+export class UdemoIconBasicComponent {
+  primerIcons = AllIcons as {
+    [key: string]: UIconDefinition;
+  };
+
+  icons: UIconDefinition[] = Object.keys(this.primerIcons).map(key => this.primerIcons[key]);
+}
