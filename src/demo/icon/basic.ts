8e6da0edf0ce70ada464333b7fa925b6f2b30cb6
ueqt <ueqtxu@gmail.com>
2020-09-16 17:05:22 +0800

    fix: icon show text

--- src/demo/icon/basic.ts
+++ src/demo/icon/basic.ts
@@ -4,7 +4,12 @@ import { AllIcons, UIconDefinition } from 'ng-ueqt';
 @Component({
   selector: 'udemo-icon-basic',
   template: `
-  <i *ngFor="let icon of icons" [uIcon]="icon.name" style="color:red;"></i>
+  <ul>
+    <li *ngFor="let icon of icons">
+        <i [uIcon]="icon.name" style="color:red;"></i>
+        <span>{{icon.name}}</span>
+    </li>
+</ul>
   <!-- <i uIcon="bars" style="color: red;"></i>
     <i uIcon="left"></i>
     <i uIcon="right"></i>
@@ -12,6 +17,40 @@ import { AllIcons, UIconDefinition } from 'ng-ueqt';
     <i uIcon="down"></i>
     <i uIcon="bars"></i> -->
     `,
+  styles: [
+    `
+    ul {
+      margin: 10px 0;
+      overflow: hidden;
+      list-style: none;
+    }
+    li {
+      position: relative;
+      float: left;
+      width: 16.66%;
+      height: 100px;
+      margin: 3px 0;
+      padding: 10px 0 0;
+      overflow: hidden;
+      color: #555;
+      text-align: center;
+      list-style: none;
+      background-color: inherit;
+      border-radius: 4px;
+      cursor: pointer;
+    }
+    i {
+      font-size: 24px;
+      margin: 12px 0 8px;
+    }
+    span {
+      display: block;
+      font-family: Lucida Console,Consolas,Monaco,Andale Mono,Ubuntu Mono,monospace;
+      white-space: nowrap;
+      text-align: center;
+    }
+    `
+  ]
 })
 export class UdemoIconBasicComponent {
   primerIcons = AllIcons as {
